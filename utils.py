658f02ec7cf2fa501d3f144d8976377eab4ef549
Ali Murad <alimurad00@hotmail.com>
2024-06-14 09:57:41 +0200

    Groq model tested

--- utils.py
+++ utils.py
@@ -7,7 +7,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import openai
 import groq
+import string
+import re
+
+from dotenv import load_dotenv, dotenv_values 
 from langchain import LanguageChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_vertexai import ChatVertexAI
+from groq import Groq
+
+#TODO: Data Cleaning using OpenAI
+#TODO: Perform Word Embedding (Not sure if it's mandatory or not)
+#TODO: Create Model Pipiline using Gemini
+#TODO: Create Model Pipiline using LLAMA2.0
+#TODO: Model Evaluation using OPENAI + Human Evaluation
+
+
+def create_gemeini_pipline:()
+
+
 
 
 
